21b2158bf127b395c9751d0f9955d9e157646138
Mark Hamrick <mmhamric@charlotte.edu>
2025-03-14 10:17:42 -0400

    Moved Variables to the top.

--- buildpod.sh
+++ buildpod.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
-#!/bin/bash
+
+PODNAME=lampstack
+MYSQL_ROOT_PASSWORD=arootpassword
+MYSQL_DATABASE=lampdb 
+MYSQL_USER=lamper
+MYSQL_PASSWORD=somepassword 
+
+
+
 echo -e "Building Apache PHP image"
 podman build -t php-apache-build .
 
 
 # Create a pod for the LAMP stack
-podman pod create --name lampstack --userns=keep-id -p 8000:8000 -p 8080:8080
+podman pod create --name  ${PODNAME}  --userns=keep-id -p 8000:8000 -p 8080:8080
 
 # Create MariaDB container
 podman run -d \
   --pod lampstack \
   --name mariadb \
-  -e MYSQL_ROOT_PASSWORD="rootpassword" \
-  -e MYSQL_USER="lampuser" \
-  -e MYSQL_PASSWORD="userpassword" \
-  -e MYSQL_DATABASE="lampdb" \
+  -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
+  -e MYSQL_USER=${MYSQL_USER} \
+  -e MYSQL_PASSWORD=${MYSQL_PASSWORD}  \
+  -e MYSQL_DATABASE=${MYSQL_DATABASE} \
   --volume ~/lampstack/lampstack/mysql:/var/lib/mysql \
   docker.io/library/mariadb:latest
 
@@ -34,6 +42,6 @@ podman run -d \
   --pod lampstack \
   --name phpmyadmin \
   -e PMA_HOST="127.0.0.1" \
-  -e MYSQL_ROOT_PASSWORD="rootpassword" \
+  -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
   -e APACHE_PORT=8080 \
   docker.io/library/phpmyadmin:latest
